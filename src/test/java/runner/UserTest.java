712f8d836151fa75536530dd1b2838fc63e8c17d
Faisal Kabir <onlyfaisalkabir@gmail.com>
2025-05-10 22:00:38 +0600

    Update User test code added

--- src/test/java/runner/UserTest.java
+++ src/test/java/runner/UserTest.java
@@ -1,5 +1,6 @@
 package runner;
 
+import com.github.javafaker.Faker;
 import config.Setup;
 import controller.UserController;
 import io.restassured.response.Response;
@@ -31,6 +32,22 @@ public class UserTest extends Setup {
         String userId = props.getProperty("userId");
 
         Response res = userController.getUserById(userId, token);
+        Assert.assertEquals(res.statusCode(), 200);
+    }
+    @Test(priority = 3)
+    public void editUserInfo() throws IOException {
+        loadProps();
+        String token = props.getProperty("adminToken");
+        String userId = props.getProperty("userId");
+        Faker faker = new Faker();
+
+        String updateBody = "{\n" +
+                "  \"firstName\": \"" + faker.name().firstName() + "\",\n" +
+                "  \"lastName\": \"" + faker.name().lastName() + "\",\n" +
+                "  \"phoneNumber\": \"" + faker.phoneNumber().subscriberNumber(11) + "\",\n" +
+                "  \"address\": \"" + faker.address().fullAddress() + "\"\n" +
+                "}";
+        Response res = userController.updateUser(userId, token, updateBody);
         Assert.assertEquals(res.statusCode(), 200);
     }
 
